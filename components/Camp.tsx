a48ed0b982afb98657b9a1d50dc96080f303bd4a
Israfil-Digi360 <mailto:israfil.hossain@digigate360.com>
2023-10-18 15:08:55 +0600

    Add Camp Section

---
+++ components/Camp.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+type Campprops = {
+  backgroundImage: string;
+  title: string;
+  subtitle: string;
+  peopleJoined: string;
+};
+const CampSite = ({
+  backgroundImage,
+  title,
+  subtitle,
+  peopleJoined,
+}: Campprops) => {
+  return <div>Camp Site</div>;
+};
+
+const Camp = () => {
+  return (
+    <section
+      className="border-2 border-green-500 2xl:max-container relative flex flex-col py-10 lg:mb-10
+    lg:py-20 xl:mb-20"
+    >
+      <div
+        className="hide-scrollbar flex h-[340px] w-full items-start 
+      justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]"
+      >
+        <CampSite />
+      </div>
+    </section>
+  );
+};
+
+export default Camp;
